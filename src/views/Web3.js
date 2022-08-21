e75ff68696c3b08211e6ed13224a8b4abcf57717
Divij Katyal <divijkatyal@gmail.com>
2022-08-21 12:42:01 +0530

    notify-fix

--- src/views/Web3.js
+++ src/views/Web3.js
@@ -1205,6 +1205,7 @@ App = {
       })
       .then(function (contributionCount) {
         var ul = document.getElementById("testtt1");
+        ul.empty();
         let cnt = 0;
         for (let i = 1; i <= contributionCount && cnt++ < 5; i++) {
           crowdsourceInstance.contributions(i).then(function (p) {
